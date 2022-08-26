0ea948a6dbafe5fe408c40754e712133348818cc
Prisha22CD <84150566+Prisha22CD@users.noreply.github.com>
2022-08-26 14:18:40 +0530

    modified (#1)
    * modified
    
    * files modified

--- CTA/Desktop/test_deskhomepage.py
+++ CTA/Desktop/test_deskhomepage.py
@@ -1,18 +1,64 @@
+from lib2to3.pgen2.driver import Driver
 import time
 from logs import *
+import requests
 from common_configration import *
 from conftest import *
-from database import *
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from utilities.Baseclass import *
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
-
-class Login(Baseclass,Database,logs):
+class Login(CTA,logs,Baseclass):
     def test_login(self):
-        self.test_log()             #log steup
-        window_size = self.driver.get_window_size() #Get Home screen window size
-        self.logger.info(window_size)
-        time.sleep(2)
-        self.test_database()
-        time.sleep(2)
+        self.test_log()             #log setup
+    
+        time.sleep(2)       
+        current_url = self.driver.current_url
+        self.logger.info("Current_Url : " + current_url)
+        response = requests.get(current_url)
+        self.logger.info("Response : " + str(response.status_code))
         
+        time.sleep(2)
+
+
+
+
+        Talk_experts = self.driver.find_element(By.XPATH,("(//button[normalize-space()='Talk to our Experts'])[1]"))
+        Talk_experts.location_once_scrolled_into_view
+        time.sleep(2)
+
+        Talk_experts.send_keys(Keys.PAGE_UP)
+        self.logger.info("CTA : " + Talk_experts.text)
+        Talk_experts.click()
+        time.sleep(3)
+        self.logger.info("CTA Class: " + Talk_experts.get_attribute("class"))
+        time.sleep(2)
+        self.cta_detail()
+        time.sleep(3)
+        
+
+        
+
+        #Whatsapp Enabled button
+
+        # Whatsapp_enabled = driver.find_element(By.XPATH,("//span[@class="slider round"]")).isEnabled())
+
+
+        # APPLY NOW
+
+        # Apply_now1 = driver.find_element(By.XPATH,"//button[normalize-space()='Apply Now'])[1]"
+
+        # Footer form
+
+        # Footer_form1 = driver.find_element(By.XPATH,"//div[normalize-space()='Want to learn more about college options and to secure an admission now!'])[1]"
+
+
+
+
+
+        
+       
--- CTA/common_configration.py
+++ CTA/common_configration.py
@@ -1,56 +1,93 @@
 import time
 import json
+from database import *
+from selenium.webdriver.common.by import By
+from logs import *
 import random
 import string
 from conftest import *
 from utilities.Baseclass import *
 from selenium.webdriver.support.ui import Select
 
-class CTA():
+class CTA(logs):
     def cta_detail(self):
         time.sleep(2)
         random_name = self.random_name()
-        self.driver.find_element_by_id("id_name_cta").send_keys(random_name)
-        self.logger.info(random_name)
+        self.driver.find_element(By.ID,"id_name_cta").send_keys(random_name)
+        self.logger.info("Name : " +random_name)
+        if not random_name:
+            error_error = driver.find_element(By.ID,"username_error")
+            print(error_error.text)
+            self.logger.info(error_error.text)
+        time.sleep(2)
 
         random_email = self.random_email()
-        self.driver.find_element_by_id("id_email_cta").send_keys(random_email)
+        self.driver.find_element(By.ID,"id_email_cta").send_keys(random_email)
         time.sleep(1)
-        self.logger.info(random_email)
+        self.logger.info("Email : " +random_email)
 
         random_number = self.random_phonenumber()
-        self.driver.find_element_by_id("id_phone_cta").send_keys(random_number)
+        self.driver.find_element(By.ID,"id_phone_cta").send_keys(random_number)
         time.sleep(2)
-        self.logger.info(random_number)
+        self.logger.info("Phone No : " +random_number)
 
         try:
-            stream = Select(self.driver.find_element_by_id("id_stream_cta"))
+            stream = Select(self.driver.find_element(By.ID,"id_stream_cta"))
             stream.select_by_index(1)
+            Stream = stream.first_selected_option
+            self.logger.info("Pref Stream : " + Stream.text)
+            time.sleep(2)
         except :
+            self.logger.info("Preferred Stream field not present")
+            time.sleep(2)
             pass 
         time.sleep(2)
         try:
-           level =  Select(self.driver.find_element_by_id("id_pref_level_cta"))
-           level.select_by_index(1) 
+           Preflevel =  Select(self.driver.find_element(By.ID,"id_pref_level_cta"))
+           Preflevel.select_by_index(1)
+           pref_Level = Preflevel.first_selected_option
+           self.logger.info("Pref Level : " + pref_Level.text)
+           time.sleep(2)
         except:
+            self.logger.info("Preferred level field not present")
+            time.sleep(2)
             pass
         time.sleep(1)
         try:
-           state = Select(self.driver.find_element_by_id("id_state_cta"))
+           state = Select(self.driver.find_element(By.ID,"id_state_cta"))
            state.select_by_index(1)
+           State = state.first_selected_option
+           self.logger.info("State : " + State.text)
+           time.sleep(2)
         except:
+            self.logger.info("State field not present")
+            time.sleep(2)
             pass
         time.sleep(1)
         try:
-           board =  Select(self.driver.find_element_by_id("id_board_cta"))
+           board =  Select(self.driver.find_element(By.ID,"id_board_cta"))
            board.select_by_index(1)
+           Board = board.first_selected_option
+           self.logger.info("Board : " + Board.text)
+           time.sleep(2)
         except:
+            self.logger.info("Board field not present")
+            time.sleep(2)
             pass
         time.sleep(1)
         self.driver.find_element_by_css_selector("button[type='submit']").click()
         time.sleep(5)
-
+        Otp = self.driver.find_element(By.CSS_SELECTOR,"//li[@class='otp_fields otp_fields_register']//input[{}].format(int(index)+1))")
+        Otp.send_keys(otp_query_1)
+       
         return CTA()
+
+        
+        cursor = conn.cursor()
+        cursor.execute(query)
+        row = cursor.fetchone()
+        result_dict = list(row)
+        self.logger.info(result_dict)
     
     
     def footer_form(self):
